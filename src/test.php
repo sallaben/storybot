739f2133dc5f97f7166402f32a4d27e316184c14
Steve Allaben <steven.allaben@gmail.com>
2015-05-05 21:24:37 -0400

    test php file for debug purposes
    run this if having issues

---
+++ src/test.php
@@ -0,0 +1,25 @@
+<pre><?php
+$enabled = false;
+
+require_once("snapchat.php");
+
+//////////// CONFIG ////////////
+$username = ""; // Your snapchat username
+$password = ""; // Your snapchat password
+$gEmail   = ""; // Gmail account
+$gPasswd  = ""; // Gmail account password
+$debug = true; // Set this to true if you want to see all outgoing requests and responses from server
+////////////////////////////////
+
+$img = "../media/logo.png"; // URL or local path to a media file (image or video)
+
+$snapchat = new Snapchat($username, $gEmail, $gPasswd, $debug);
+
+//Login to Snapchat with your username and password
+$snapchat->login($password);
+
+// Set a story
+$snapchat->setStory($img);
+
+$snapchat->closeAppEvent();
+?></pre>
